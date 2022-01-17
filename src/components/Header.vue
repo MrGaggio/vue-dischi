1fbc125d7d26f5fbd4abd6a7b5731c523c5b732c
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-17 16:46:32 +0000

    inserito header, main e logo

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div class="logo_container">
+            <img class="logo" src="../assets/logo.png" alt="">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .logo_container {
+        background-color:  #2E3A46;
+        height: 80px;
+        display: flex;
+        padding-left: 20px;
+    }
+    .logo {
+        padding: 15px;
+    }
+</style>
+/Users/stefanoruggiu/Desktop/Git/vue-dischi/src/assets/spotify.png
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script >
+
+<style>
+    main {
+        background-color: #1E2D3B;
+        height: 1000px;
+    }
+</style>
